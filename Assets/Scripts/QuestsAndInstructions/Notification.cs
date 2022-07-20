49f2c4ed8f74e541013da29e7609fbeae58fbe82
Rishav Banerjee <thatamuzak@gmail.com>
2022-07-20 16:52:42 -0600

    Updated Textures, Added Skeleton Frameworks

---
+++ Assets/Scripts/QuestsAndInstructions/Notification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notification : MonoBehaviour
+{
+    [SerializeField] private string text;
+    [SerializeField] private Image image;
+    [SerializeField] private GameObject model;
+    [SerializeField] private AudioClip sound;
+    [SerializeField] private bool playSoundOnStart;
+    [SerializeField] private bool requiresConfirmation;
+
+    private void Start()
+    {
+        if (sound != null && playSoundOnStart)
+        {
+            SoundManager.Instance.PlaySound(sound);
+        }
+    }
+    
+    public void PlaySound()
+    {
+        if (sound != null)
+        {
+            SoundManager.Instance.PlaySound(sound);
+        }
+        else
+        {
+            throw new Exception("Sound is null");
+        }
+    }
+}
---
+++ Assets/Scripts/UIandSFX/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource musicSource, sfxSource;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        musicSource.loop = true;
+        if(musicSource.clip!=null)
+            musicSource.Play();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
