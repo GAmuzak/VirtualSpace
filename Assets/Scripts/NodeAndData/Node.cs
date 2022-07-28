bba5d49c05dccbf151c4cd6472b9cb2e2980de77
Rishav Banerjee <thatamuzak@gmail.com>
2022-07-27 21:45:34 -0600

    AddedNodeLogging

--- Assets/Scripts/NodeAndData/Node.cs
+++ Assets/Scripts/NodeAndData/Node.cs
@@ -1,28 +1,22 @@
+using System;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
     public bool HasPlayer { get; private set; } = false;
     
-    [SerializeField] private string nodeName;
-    
     public void OnTriggerEnter(Collider other)
-    {   
+    {
         if (!other.gameObject.CompareTag("Player")) return;
-        Debug.Log("Player entered node " + name);
+        NodeManager.Instance.Entered(this);
         HasPlayer = true;
     }
     
     public void OnTriggerExit(Collider other)
     {
         if (!other.gameObject.CompareTag("Player")) return;
-        Debug.Log("Player exited node " + name);
+        NodeManager.Instance.Exited(this);
         HasPlayer = false;
-    }
-
-    public void LogMovement()
-    {
-        
     }
 }
 
