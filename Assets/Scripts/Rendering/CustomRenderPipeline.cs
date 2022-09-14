e3dadfa8269a25d8a8e6c32bba396b0a8a115d75
Rishav Banerjee <thatamuzak@gmail.com>
2022-09-14 16:26:48 -0600

    Custom Render Pipeline

---
+++ Assets/Scripts/Rendering/CustomRenderPipeline.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline:RenderPipeline
+{
+    private CameraRenderer renderer = new CameraRenderer();
+
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach (Camera camera in cameras) {
+            renderer.Render(context, camera);
+        }
+    }
+}
