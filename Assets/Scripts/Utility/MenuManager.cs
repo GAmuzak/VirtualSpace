822d3a1cbf1d74c8aa5103a2d370283acb7b2a01
Rishav Banerjee <thatamuzak@gmail.com>
2022-09-17 19:42:17 -0600

    MainMenuFinished

--- Assets/Scripts/Utility/MenuManager.cs
+++ Assets/Scripts/Utility/MenuManager.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
